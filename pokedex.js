c412eff121c394ce48e2b203b892dd9f5fc660d7
Carlos García <carlos.garciamor@nube.unadmexico.mx>
2022-03-24 05:34:42 -0600

    with charts

--- pokedex.js
+++ pokedex.js
@@ -1,3 +1,39 @@
+const normalType = {r: 168, g: 167, b: 122}
+const fireType = {r: 238, g: 129, b: 48}
+const labels = [
+  "HP",
+  "Attack",
+  "Defense",
+  "Speed",
+  "Special-Defense",
+  "Special-Attack"
+];
+let data = {
+  labels: labels,
+  datasets: [{
+    label: 'Estadisticas base',
+    backgroundColor: `rgba(${normalType.r}, ${normalType.g}, ${normalType.b}, .8)`,
+    borderColor: `rgba(${fireType.r}, ${fireType.g}, ${fireType.b}, 1)`,
+    data: [0, 0, 0, 0, 0, 0],
+  }]
+};
+let config = {
+  type: 'radar',
+  data: data,
+  options:  {
+    scales: {
+      r: {
+        suggestedMin: 0,
+        suggestedMax: 250
+      }
+    }
+  }
+};
+let pokeStats = new Chart(
+  document.getElementById('pokeStats'),
+  config
+);
+let pokeFetch = {};
 let statusColor = document.getElementById('status');
 let pokeSearch = document.getElementById('pokeSearch');
 let pokeSwap = document.getElementById('pokeSwap');
@@ -6,7 +42,6 @@ let pokeBG = document.querySelector('.graphics')
 let type1 = document.getElementById('type1');
 let type2 = document.getElementById('type2');
 
-
 function setData (pokeObj) {
   let typeBG = `./media/Types/${pokeObj.types[0].type.name}.jpg`
   let pokeImg = pokeObj.sprites.front_default;
@@ -14,12 +49,11 @@ function setData (pokeObj) {
   document.getElementById('pokeName').innerHTML = pokeObj.species.name;
   pokePhoto.setAttribute("src", pokeImg);
   pokeBG.style.backgroundImage = `url(${typeBG})`;
-
   type1.innerHTML = pokeObj.types[0].type.name;
   type1.style.backgroundColor = `var(--${pokeObj.types[0].type.name})`;
   if(pokeObj.types.length > 1) {
-    type2.innerHTML = pokeObj.types[1].type.name;
     type2.style.display = "block";
+    type2.innerHTML = pokeObj.types[1].type.name;
     type2.style.backgroundColor = `var(--${pokeObj.types[1].type.name})`;
   }
   else {
@@ -27,14 +61,24 @@ function setData (pokeObj) {
   }
   console.log(type2);
 }
-  
+
 function setErrorData () {
   const defaultImg = "./media/WhosThatPoke.png";
   document.getElementById('pokeName').innerHTML = "Quien es ese Pokemon?";
   pokePhoto.setAttribute("src", defaultImg);
   pokeBG.style.backgroundImage = `url("./media/RotomHeaderEmptyblink.png")`
 }
-
+function setStats (pokeObj) {
+  data.datasets[0].data = [
+    pokeObj.stats[0].base_stat,
+    pokeObj.stats[1].base_stat,
+    pokeObj.stats[2].base_stat,
+    pokeObj.stats[5].base_stat,
+    pokeObj.stats[4].base_stat,
+    pokeObj.stats[3].base_stat
+  ];
+  pokeStats.update();
+}
 function blinkStatus (fetchStatus) {
   let statusImg = "";
   switch(fetchStatus) {
@@ -65,13 +109,14 @@ function fetchPokemon (poke) {
     if(res.status != '200') {
       console.log(res);
     } else {
-      console.log(res);
       return res.json();
     }
   }).then((data) => {
     blinkStatus('o');
     console.log(data);
+    pokeFetch = data;
     setData(data);
+    setStats(data);
   }).catch((error) => {
     blinkStatus('e');
     console.log(error);
